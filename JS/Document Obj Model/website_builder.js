5548020d69822542304146d583ad6970bf707190
Shashank Bharti <shashank_24a12res620@iitp.ac.in>
2025-04-25 01:28:18 +0530

    add video num css

--- JS/Document Obj Model/website_builder.js
+++ JS/Document Obj Model/website_builder.js
@@ -1,9 +1,9 @@
-function createCard(title, cname, views, thumbnail, monthsOld, duration) {
+function createCard(title, cname, views, thumbnail, monthsOld, duration,v_num) {
   let a; 
   if (views >= 1000000) {
-    a = Math.round((views / 1000000)) + 'M'; 
+    a = (views / 1000000).toFixed(1) + 'M'; 
   } else if (views >= 1000) {  
-    a = Math.round((views / 1000))+ 'K'; 
+    a = (views / 1000).toFixed(1)+ 'K'; 
   } else {
     a = views; 
   }
@@ -12,6 +12,7 @@ function createCard(title, cname, views, thumbnail, monthsOld, duration) {
 
   let card = document.createElement("div");
   card.innerHTML = `
+  <div class = "v_num">${v_num}</div>
   <div class="thumb_and_time">
     <img class="thumb" src=${thumbnail} alt="Thumbnail" />
     <div class="time">${duration}</div>
@@ -25,6 +26,6 @@ function createCard(title, cname, views, thumbnail, monthsOld, duration) {
     .insertAdjacentElement("beforeBegin", card);
 }
 
-createCard('Installing VS Code & How Websites Work | Sigma Web Development Course - Tutorial #1', 'CodeWithHarry', 4400000, 'thumbnail.webp', '1 year', '31:20');
-createCard('Your First HTML Website | Sigma Web Development Course - Tutorial #2', 'CodeWithHarry', 16000, 'thumbnail.webp', '1 year', '28:31');
-createCard('Basic Structure of an HTML Website | Sigma Web Development Course - Tutorial #3', 'CodeWithHarry', 1000000, 'thumbnail.webp', '1 year', '11:21');
+createCard('Installing VS Code & How Websites Work | Sigma Web Development Course - Tutorial #1', 'CodeWithHarry', 4446546, 'thumbnail.webp', '1 year', '31:20',1);
+createCard('Your First HTML Website | Sigma Web Development Course - Tutorial #2', 'CodeWithHarry', 1619456, 'thumbnail.webp', '1 year', '28:31',2);
+createCard('Basic Structure of an HTML Website | Sigma Web Development Course - Tutorial #3', 'CodeWithHarry', 1146522, 'thumbnail.webp', '1 year', '11:21',3);
